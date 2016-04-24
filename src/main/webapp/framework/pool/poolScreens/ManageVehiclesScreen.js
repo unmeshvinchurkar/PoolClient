21cd6167189891612b4d42f5dc9a9496458c3964
unmeshvinchurkar <unmeshv02@gmail.com>
2016-04-24 14:39:23 +0530

    Manage vehicle page

--- src/main/webapp/framework/pool/poolScreens/ManageVehiclesScreen.js
+++ src/main/webapp/framework/pool/poolScreens/ManageVehiclesScreen.js
@@ -25,6 +25,8 @@ PROJECT.namespace("PROJECT.pool.poolScreens");
 		var _containerElemId = containerElemId
 
 		var _container = null;
+		var _validator = null;
+		var _doesVehicleExist = false;
 
 		/* Public Properties */
 		objRef.render = render;
@@ -38,8 +40,7 @@ PROJECT.namespace("PROJECT.pool.poolScreens");
 			_container = $('#' + _containerElemId);
 			_container.html(data);
 			_loadVehicleData();
-
-			$("#submit").click(_handleClick);
+			$("#editDetails").click(_handleVehicleDetails);
 		}
 
 		function _loadVehicleData() {
@@ -48,26 +49,97 @@ PROJECT.namespace("PROJECT.pool.poolScreens");
 
 			function fillData(data) {
 				if (data) {
-					$("#VehicledetailManufacturer").val(data.manufacturer);
-					$("#VehicledetailModelname").val(data.model);
-					$("#VehicledetailFueltype").val(data.fuelType);
-					$("#VehicledetailColour").val(data.color);
-					$("#VehicledetailRegistrationNumber").val(
-							data.registrationNo);
-					$("#VehicledetailDrivingLicense").val(data.drivingLicense);
+					var editedFields = $("#manufacturer, #modelname, #fueltype, #color, #registrationNumber, #drivingLicense");
+					
+					editedFields.removeAttr("readonly");
+					_doesVehicleExist = true;
+					$("#manufacturer").val(data.manufacturer);
+					$("#modelname").val(data.model);
+					$("#fueltype").val(data.fuelType);
+					$("#color").val(data.color);
+					$("#registrationNumber").val(data.registrationNo);
+					$("#drivingLicense").val(data.drivingLicense);
+					editedFields.attr("readonly", true);
+				} else {
+					_handleVehicleDetails();
 				}
 			}
 		}
 
-		function _handleClick(e) {
+		function _handleVehicleDetails() {
+
+			var readonly = $("#manufacturer").attr("readonly");
+			var editedFields = $("#manufacturer, #modelname, #fueltype, #color, #registrationNumber, #drivingLicense");
+			$("small[id$='_error']").remove();
+
+			if (readonly == "readonly") {
+
+				editedFields.removeAttr("readonly");
+
+				_validator = new FormValidator('vehicleForm', [ {
+					name : 'manufacturer',
+					display : 'Manufacturer',
+					rules : 'required|max_length[20]|alpha'
+				}, {
+					name : 'modelname',
+					display : 'Model Name',
+					rules : 'required|max_length[20]'
+				}, {
+					name : 'fueltype',
+					display : 'Fuel Type',
+					rules : 'required|alpha|max_length[10]'
+				}, {
+					name : 'color',
+					rules : 'required|alpha|max_length[20]'
+				}, {
+					name : 'registrationNumber',
+					rules : 'required|alpha_numeric|max_length[10]'
+				}, {
+					name : 'drivingLicense',
+					rules : 'required|alpha_numeric|max_length[30]'
+				} ], function(errors, event) {
+
+					$("small[id$='_error']").remove();
+
+					if (errors.length > 0) {
+						for (var i = 0; i < errors.length; i++) {
+							var $span = $('<small/>').attr("id",
+									errors[i].id + "_error").addClass(
+									'help-block errorMessage').insertAfter(
+									$(errors[i].element)).html(
+									errors[i].message);
+						}
+
+					} else {
+						_saveVehicle();
+					}
+				});
+
+				$("#editDetails").html("Cancel Edit");
+
+				$("#vehicleForm").append(
+						'<input type="button" id="save" class="btn btn-custom btn-lg btn-block"'
+								+ 'value="Save" />');
+
+				$("#save").click(function(e) {
+					_validator.form.onsubmit();
+				});
+
+			} else {
+				editedFields.attr("readonly", true);
+				$("#editDetails").html("Edit Details");
+				$("#save").remove();
+			}
+		}
+
+		function _saveVehicle() {
 			var params = {};
-			params["manufacturer"] = $("#VehicledetailManufacturer").val();
-			params["model"] = $("#VehicledetailModelname").val();
-			params["fuelType"] = $("#VehicledetailFueltype").val();
-			params["color"] = $("#VehicledetailColour").val();
-			params["registrationNumber"] = $("#VehicledetailRegistrationNumber")
-					.val();
-			params["drivingLicense"] = $("#VehicledetailDrivingLicense").val();
+			params["manufacturer"] = $("#manufacturer").val();
+			params["model"] = $("#modelname").val();
+			params["fuelType"] = $("#fueltype").val();
+			params["color"] = $("#color").val();
+			params["registrationNumber"] = $("#registrationNumber").val();
+			params["drivingLicense"] = $("#drivingLicense").val();
 
 			objRef.fireCommand("addVehicle",
 					[ params, _saveSuccess, _saveError ]);
