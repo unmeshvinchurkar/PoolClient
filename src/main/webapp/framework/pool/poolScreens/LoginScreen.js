89bd0a916de07de0960e58946b94cf5f29f9dff3
unmeshvinchurkar <unmeshv02@gmail.com>
2016-06-22 22:55:27 +0530

    Facebook login

--- src/main/webapp/framework/pool/poolScreens/LoginScreen.js
+++ src/main/webapp/framework/pool/poolScreens/LoginScreen.js
@@ -24,10 +24,66 @@ PROJECT.namespace("PROJECT.pool.poolScreens");
 
 		/* Public Properties */
 		objRef.render = render;
-		objRef.handleClick = handleClick;
+
+		function _fb_login() {
+
+			FB
+					.login(function(response) {
+
+						if (response.authResponse) {
+							var aToken = response.authResponse.accessToken;
+							var user_id = response.authResponse.userID;
+							_fetchUserDetails(aToken);
+						} else {
+							// user hit cancel button
+							console
+									.log('User cancelled login or did not fully authorize.');
+						}
+					});
+		}
+
+		function _fetchUserDetails(aToken) {
+			FB.api('/me', 'get', {
+				access_token : aToken,
+				fields : 'picture,about,work,id,name,gender,email,birthday'
+			}, function(response) {
+				_saveUserDetails(response);
+
+			});
+		}
 
 		function render() {
-			SegmentLoader.getInstance().getSegment("loginSeg.xml", null, _init);
+
+			if (!window.fbAsyncInit) {
+				window.fbAsyncInit = function() {
+					FB.init({
+						appId : '145263072549453',
+						oauth : true,
+						status : true, // check login status
+						cookie : true, // enable cookies to allow the server to
+						xfbml : true, // parse XFBML
+						version : 'v2.6'
+					});
+
+					SegmentLoader.getInstance().getSegment("loginSeg.xml",
+							null, _init);
+				};
+
+				(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) {
+						return;
+					}
+					js = d.createElement(s);
+					js.id = id;
+					js.src = "//connect.facebook.net/en_US/sdk.js";
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+			} else {
+				SegmentLoader.getInstance().getSegment("loginSeg.xml", null,
+						_init);
+			}
+
 		}
 
 		function _init(data) {
@@ -35,18 +91,21 @@ PROJECT.namespace("PROJECT.pool.poolScreens");
 			_container.html("");
 			_container.html(data);
 
-			$("#registerUser").click(_registerUser);
-			$("#loginId").click(handleClick);
+			$("#loginId").click(_fb_login);
 		}
 
 		function _registerUser(e) {
 			objRef.navigateTo(PoolConstants.USER_REGISTRATION_SCREEN);
 		}
 
-		function handleClick(e) {
+		function _saveUserDetails(response) {
 			var params = {};
-			params["username"] = $("#usernameId").val();
-			params["password"] = $("#passwordId").val();
+			params["email"] = response.email;
+			params["birthday"] = response.birthday;
+			params["gender"] = response.gender;
+			params["facebookId"] = response.id;
+			params["name"] = response.name;
+			params["pictureUrl"] = response.picture.data.url;
 
 			objRef.fireCommand(PoolConstants.LOGIN_COMMAND, [ params, _login,
 					_loginFailed ]);
@@ -59,8 +118,7 @@ PROJECT.namespace("PROJECT.pool.poolScreens");
 		function _loginFailed() {
 			$("#passwordId_error").remove();
 			var $span = $('<small/>').attr("id", "passwordId_error").addClass(
-			'help-block errorMessage').insertAfter(
-			$($("#passwordId")));
+					'help-block errorMessage').insertAfter($($("#passwordId")));
 
 			$span.html("Login or password didn't match");
 		}
